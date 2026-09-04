51fbd095dcdf4754eb36dc81037eb96679b45d54
HeyChaddie <cph-rs286@stud.ek.dk>
2026-09-04 19:41:57 +0200

    update exercises

--- theme_switcher/script.js
+++ theme_switcher/script.js
@@ -1,14 +1,22 @@
 const colors = {
-  dark: { theme: "dark", location: "body" },
-  light: { theme: "light", location: "body" },
-  fun: { theme: "fun", location: "body" },
+  dark: { theme: "dark" },
+  light: { theme: "light" },
+  fun: { theme: "fun" },
 };
 
 const select = document.querySelector("select");
 select.addEventListener("change", colorChange);
 
-function colorChange() {
+function colorChange(evt) {
   const selectedColor = colors[select.value];
 
   document.querySelector("[data-theme]").setAttribute("data-theme", selectedColor.theme);
+
+  setTheme(evt.target.value);
+}
+
+function setTheme(theme) {
+  console.log("theme", theme);
+  localStorage.setItem("tsTheme", theme);
+  document.querySelector("[data-theme]").dataset.theme = theme;
 }
